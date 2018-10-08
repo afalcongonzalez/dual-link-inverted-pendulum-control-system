8aba902253b5c951f9bab7ca7855eb314f86fc1f
AndyFTS <andyf280@gmail.com>
2018-10-08 14:37:05 -0400

    correct simulink demo

--- Phase 1-Dynamics/phase_1_calculations.m
+++ Phase 1-Dynamics/phase_1_calculations.m
@@ -36,7 +36,7 @@ N_q_qdot = [n1;n2];
 tau = [tau1;0];
 
 euler_lagrange = d_q*qdotdot + N_q_qdot == tau;
-isolate(euler_lagrange(1,1),q1dotdot);
+% isolate(euler_lagrange(1,1),q1dotdot);
 Q1DotDot = rhs(isolate(subs(isolate(euler_lagrange(1,1),q1dotdot),q2dotdot,rhs(isolate(euler_lagrange(2,1),q2dotdot))),q1dotdot));
 Q2DotDot = rhs(isolate(subs(isolate(euler_lagrange(1,1),q2dotdot),q1dotdot,rhs(isolate(euler_lagrange(2,1),q1dotdot))),q2dotdot));
 syms k u1 u2 x1dot x2dot x3dot x4dot x1 x2 x3 x4; 
@@ -45,5 +45,13 @@ X = [ x1; x2 ;x3;x4];
 Tau = [u1;u2];
 A = jacobian(Xdot,X);
 B = jacobian(Xdot,Tau);
-A1 = subs(A,[x1 x2 x3 x4],[pi 0 0 0]);
-B1 = subs(B,[x1 x2 x3 x4],[pi 0 0 0]);
+
+Abar = double(subs(A,[x1 x2 x3 x4 m1 m2 I1 I2 lc1 lc2 l1 g bf1 bf2 k],[pi/2 0 0 0 0.03 0.03 0.05 0.05 0.04 0.04 0.08 9.8 0.03 0.006 0.5]));
+Bbar =double(subs(B,[x1 x2 x3 x4 m1 m2 I1 I2 lc1 lc2 l1 g bf1 bf2 k],[pi/2 0 0 0 0.03 0.03 0.05 0.05 0.04 0.04 0.08 9.8 0.03 0.006 0.5]));
+C =  [ 1 0 0 0;0 1 0 0; 0 0 0 0; 0 0 0 0];
+D = 0;
+A1 = subs(A,[x1 x2 x3 x4],[pi/2 0 0 0]);
+B1 = subs(B,[x1 x2 x3 x4],[pi/2 0 0 0]);
+A_ = subs(A1,[m1 m2 I1 I2 lc1 lc2 l1 g bf1 bf2 k],[0.03 0.03 0.05 0.05 0.04 0.04 0.08 9.8 0.4 0.6 0.5]);
+B_ = subs(B1,[m1 m2 I1 I2 lc1 lc2 l1 g bf1 bf2 k],[0.03 0.03 0.05 0.05 0.04 0.04 0.08 9.8 0.4 0.6 0.5]);
+system = ss(double(A_),double(B_(:,1)),[ 1 0 0 0;0 1 0 0; 0 0 1 0; 0 0 0 1],0);
